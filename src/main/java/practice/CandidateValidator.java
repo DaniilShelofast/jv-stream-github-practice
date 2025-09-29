d6cd0f2c120fef3718831f05c9d6984df26ab3c0
Danyil <shelofast.daniil@gmail.com>
2025-09-29 15:47:58 +0300

    work with stream api

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,17 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] date = candidate.getPeriodsInUkr().split("-");
+        int timeInUkraine = Integer.parseInt(date[1]) - Integer.parseInt(date[0]);
+        return candidate.getAge() >= 35
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && timeInUkraine >= 10;
+    }
 }
